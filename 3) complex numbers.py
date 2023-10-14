3459c6e2706fa9a407e2298a35931a30740a1e54
Mahdi Ahmadi Kamal <909stars@gmail.com>
2023-10-14 18:39:31 +0330

    My first commit

--- 3) complex numbers.py
+++ 3) complex numbers.py
@@ -7,12 +7,39 @@ class Complex:
     def show(self):
         print(self.real, "+", self.image, "i")
     
-    def sum():
-        ...
+    def sum(self, other):
+        new_real = self.real + other.real
+        new_image = self.image + other.image
+        
+        new_com = Complex(new_real, new_image)
+        return new_com
 
-    def sub():
-        ...
+    def sub(self, other):
+        new_real = self.real - other.real
+        new_image = self.image - other.image
+        
+        new_com = Complex(new_real, new_image)
+        return new_com
+    
+    def mul(self, other):
+        new_real = (self.real * other.real) - (self.image * other.image)
+        new_image = (self.image * other.real) + (self.real * other.image)
+
+        new_com = Complex(new_real, new_image)
+        return new_com
 
 
 a = Complex(5, 8)
 a.show()
+
+b = Complex(2, 3)
+b.show()
+
+c = a.sum(b)
+c.show()
+
+d = a.sub(b)
+d.show()
+
+e = a.mul(b)
+e.show()
